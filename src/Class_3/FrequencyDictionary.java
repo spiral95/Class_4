7aec0b378ccf042e0956dd432c2456056cac4e48
spiral95 <spiral95@mail.ru>
2022-07-04 09:21:43 +0300

    04.07/22-1

--- src/Class_3/FrequencyDictionary.java
+++ src/Class_3/FrequencyDictionary.java
@@ -3,7 +3,7 @@ package Class_3;
 public class FrequencyDictionary {
 
     public static void main(String[] args) {
-        String[] sentence = {"Hello", "World", "Hello, World","Hello, World","Hello, World","Hello, World"};
+        String[] sentence = {"Hello", "World", "Hello", "World","Hello", "World","Hello", "World","Hello", "World"};
 
         String word1 = "Hello";
         String word2 = "World";
@@ -14,7 +14,6 @@ public class FrequencyDictionary {
             if(word1.equals(sentence[i])) {
                 sum++;
             }
-            System.out.println(sum);
         }
         System.out.println("The Word \"Hello\" - " + sum);
     }
