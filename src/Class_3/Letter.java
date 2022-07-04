704ed159bf5ed4ebd015e09ad949f76091f8d475
spiral95 <spiral95@mail.ru>
2022-07-04 09:17:12 +0300

    04.07/22

---
+++ src/Class_3/Letter.java
@@ -0,0 +1,25 @@
+package Class_3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Letter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String name = scanner.nextLine();
+        String[] mass = {"A","B","C","D","E","F"};
+        int i = random.nextInt(mass.length);
+        String name1 = mass[i];
+
+        System.out.println(name1);
+
+        if(name == name1){
+            System.out.println("You're the Winner");
+        }
+
+    }
+}
