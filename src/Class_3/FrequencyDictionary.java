c16b2adae01cf4d11943afa31c760266fb85106f
spiral95 <spiral95@mail.ru>
2022-07-10 14:42:09 +0300

    10.07/22-1

--- src/Class_3/FrequencyDictionary.java
+++ src/Class_3/FrequencyDictionary.java
@@ -9,15 +9,13 @@ public class FrequencyDictionary {
         String sentence = "I love using Java because it is such an elegant language with so many purposes and such a bright future";
         String[] sentenceArray = sentence.split(" +");
 
-        int sum = 1;
-        for (int k = 1, i = 0; k < sentenceArray.length; k++) {
+        int sum = 0;
+        int i = 0;
+        for (int k = 0; k < sentenceArray.length; k++) {
             if (sentenceArray[i].equals(sentenceArray[k])) {
                 sum++;
-            } else {
-                sum = 1;
-                i++;
             }
-            System.out.println("This word - " + sentenceArray[i] + " - occurs - " + sum + " - times in the sentence!");
         }
+        System.out.println("This word - " + sentenceArray[i] + " - occurs - " + sum + " - times in the sentence!");
     }
 }
