ae6a9b71c3e3290ce643f5c3af3f7ce9011ee4b8
spiral95 <spiral95@mail.ru>
2022-07-31 14:10:43 +0300

    31.07/22-1

--- src/Class_11/Plus.java
+++ src/Class_11/Plus.java
@@ -2,19 +2,19 @@ package Class_11;
 
 // Задание: Создать класс, который может складывать и числа и строки.
 
-public class Plus<T>{
+public class Plus<T> {
 
-    public T method(T type){
-        return type;
+    public T method(T type) {
+            return type;
+        }
+
+        public static void main (String[]args){
+
+            Plus<String> plus = new Plus<String>();
+            Plus<Integer> integerPlus = new Plus<Integer>();
+
+            System.out.println(plus.method("Test") + integerPlus.method(10));
+        }
+
     }
-
-    public static void main(String[] args) {
-        Plus<String> plus = new Plus<String>();
-        Plus<Integer> integerPlus = new Plus<Integer>();
-
-        plus.method("Test");
-        integerPlus.method(10);
-
-    }
-
 }
